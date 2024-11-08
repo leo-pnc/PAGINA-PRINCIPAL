5770238cb40bca14ead9a8ecad4e73791bf275ee
leo-pnc <leonelponce0001@gmail.com>
2024-11-08 13:45:40 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Observador de intersección para manejar la visibilidad de los regalos al hacer scroll
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -15,7 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
         threshold: 0.1
     });
 
+    // Observa todos los elementos con la clase 'regalo'
     document.querySelectorAll('.regalo').forEach(regalo => {
         observer.observe(regalo);
+
+        // Agregar evento de clic para cada regalo
+        regalo.addEventListener('click', () => {
+            // Alternar la clase 'clicked' para mostrar los elementos dentro de la promo
+            regalo.classList.toggle('clicked');
+        });
     });
 });
